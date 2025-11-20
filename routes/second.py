89b61a626490dece69aac9d1458f253b50df2dfa
Shahab Avin <shahab1992@hotmail.com>
2025-11-20 12:39:58 -0500

    taghire alaki

--- routes/second.py
+++ routes/second.py
@@ -5,5 +5,5 @@ second_bp = Blueprint('second', __name__)
 @second_bp.route("/second")
 def secondroute():
     titleman = "Our second page"
-    message = "This is the end of world"
+    message = "This is the end of world ♥"
     return render_template("second_template.html", title=titleman, message=message)
