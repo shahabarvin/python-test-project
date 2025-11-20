e911192e3e5ef008846a3c2031ea998cd84e8df4
Amin88-hub <amin.1988.hn@gmail.com>
2025-11-20 12:35:31 -0500

    Update Second.py

--- routes/second.py
+++ routes/second.py
@@ -5,5 +5,5 @@ second_bp = Blueprint('second', __name__)
 @second_bp.route("/second")
 def secondroute():
     titleman = "Our second page"
-    message = "این یک پیام نمونه است"
+    message = "This is the end of world"
     return render_template("second_template.html", title=titleman, message=message)
